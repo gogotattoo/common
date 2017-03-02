0ceeb9b50a5bd71604a7f212e91141d6d9b6306e
Roman <roman@tronenko.net>
2017-03-02 17:35:33 +0800

    tattoo and artwork structs

---
+++ models/artwork.go
@@ -0,0 +1,22 @@
+package models
+
+type Artwork struct {
+	ID    string `json:"id,omitempty"`
+	Link  string `json:"link,omitempty"`
+	Title string `json:"title,omitempty" toml:"title"`
+	//MadeLocation Address  `json:"made_at"`
+	DurationMin int    `json:"duration_min,omitempty"`
+	Gender      string `json:"gender,omitempty"`
+	Extra       string `json:"extra,omitempty"`
+	Article     string `json:"article,omitempty"`
+
+	MadeDate        string   `json:"tattoodate,omitempty" toml:"tattoodate"`
+	PublishDate     string   `json:"date,omitempty"`
+	Tags            []string `json:"tags,omitempty"`
+	BodyParts       []string `json:"bodypart,omitempty"`
+	ImageIpfs       string   `json:"image_ipfs" toml:"image_ipfs"`
+	ImagesIpfs      []string `json:"images_ipfs,omitempty" toml:"images_ipfs"`
+	LocationCity    string   `json:"made_at_city" toml:"location_city"`
+	LocationCountry string   `json:"made_at_country" toml:"location_country"`
+	MadeAtShop      string   `json:"made_at_shop,omitempty" toml:"made_at_shop"`
+}
---
+++ models/tattoo.go
@@ -0,0 +1,12 @@
+package models
+
+type tattoo struct {
+	Artwork
+}
+
+// Address stores the location information where the work was made
+// type Address struct {
+// 	City    string `json:"city,omitempty"`
+// 	Country string `json:"country,omitempty"`
+// 	Shop    string `json:"shop,omitempty"`
+// }
